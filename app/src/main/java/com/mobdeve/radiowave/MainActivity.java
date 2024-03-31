d027419bf96633c81ff7d64cc00a8c875ba0d517
Marco Gonzalez <marco_gonzalez@dlsu.edu.ph>
2024-03-31 20:29:22 +0800

    AMlist (90% complete)

--- app/src/main/java/com/mobdeve/radiowave/MainActivity.java
+++ app/src/main/java/com/mobdeve/radiowave/MainActivity.java
@@ -1,14 +1,36 @@
 package com.mobdeve.radiowave;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.List;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.amlistpage);//CHANGE THIS TO activity_main once menu page is done
+
+        RecyclerView AMrecyclerview = findViewById(R.id.AMrecyclerview);
+
+        AMradiostationsClass[] amradiostations = new AMradiostationsClass[]{
+                new AMradiostationsClass("TeleRadyo","www.link1.com"),
+                new AMradiostationsClass("Super Radyo DZBB","www.link2.com"),
+                new AMradiostationsClass("Radio Veritas","www.link3.com"),
+                new AMradiostationsClass("Radyo La Verdad","www.link4.com"),
+        };
+
+        AMrecyclerview.setLayoutManager(new LinearLayoutManager(this));
+        AMrecyclerview.setAdapter(new MyAdapter(getApplicationContext(),amradiostations));
     }
+
 }
