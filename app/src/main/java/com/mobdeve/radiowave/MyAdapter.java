e15688aaa93fea9914170bb8092971fe28d4bfeb
aajrong342 <141275124+aajrong342@users.noreply.github.com>
2024-04-01 03:55:03 +0800

    Fixed the Recycler Views

--- app/src/main/java/com/mobdeve/radiowave/MyAdapter.java
+++ app/src/main/java/com/mobdeve/radiowave/MyAdapter.java
@@ -7,36 +7,55 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MyAdapter extends RecyclerView.Adapter<ViewHolder> {
-    Context context;
-    public AMradiostationsClass[] amradiostations;
+import android.widget.ImageView;
+import android.widget.TextView;
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private Context context;
+    private AMradiostationsClass[] amradiostations;
+
     public MyAdapter(Context context, AMradiostationsClass[] amradiostations) {
         this.context = context;
         this.amradiostations = amradiostations;
     }
 
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-
-        View view = LayoutInflater.from(context).inflate(R.layout.amlistpagerecyclerview,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.amlistpagerecyclerview, parent, false);
         return new ViewHolder(view);
     }
 
-
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.tvAMstationname.setText(amradiostations[position].getstationname());
-        holder.tvAMstationlink.setText(amradiostations[position].getstationlink());
+        holder.bind(position);
     }
-
 
     @Override
     public int getItemCount() {
         return amradiostations.length;
     }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvAMstationname;
+        TextView tvAMstationlink;
+        ImageView imageView; // Add ImageView
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvAMstationname = itemView.findViewById(R.id.tvAMstationname);
+            tvAMstationlink = itemView.findViewById(R.id.tvAMstationlink);
+            imageView = itemView.findViewById(R.id.imageView); // Initialize ImageView
+        }
+
+        public void bind(int position) {
+            AMradiostationsClass station = amradiostations[position];
+            // Set the station name
+            tvAMstationname.setText(station.getstationname());
+            // Set the station link
+            tvAMstationlink.setText(station.getstationlink());
+
+            // Set the image resource
+            imageView.setImageResource(R.drawable.rvimgstationcontainer);
+        }
+    }
 }
-
-
--- app/src/main/java/com/mobdeve/radiowave/MyAdapterFM.java
+++ app/src/main/java/com/mobdeve/radiowave/MyAdapterFM.java
@@ -6,37 +6,56 @@ import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MyAdapterFM extends RecyclerView.Adapter<ViewHolderFM> {
-    Context context;
-    public FMradiostationsClass[] fmradiostations;
+public class MyAdapterFM extends RecyclerView.Adapter<MyAdapterFM.ViewHolder> {
+    private Context context;
+    private FMradiostationsClass[] fmradiostations;
+
     public MyAdapterFM(Context context, FMradiostationsClass[] fmradiostations) {
         this.context = context;
         this.fmradiostations = fmradiostations;
     }
 
-
     @NonNull
     @Override
-    public ViewHolderFM onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
-
-        View view = LayoutInflater.from(context).inflate(R.layout.fmlistpagerecyclerview,parent,false);
-        return new ViewHolderFM(view);
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.fmlistpagerecyclerview, parent, false);
+        return new ViewHolder(view);
     }
-
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolderFM holder, int position) {
-        holder.tvFMstationname.setText(fmradiostations[position].getstationname());
-        holder.tvFMstationlink.setText(fmradiostations[position].getstationlink());
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.bind(position);
     }
-
 
     @Override
     public int getItemCount() {
         return fmradiostations.length;
     }
-}
 
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView tvFMstationname;
+        TextView tvFMstationlink;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            tvFMstationname = itemView.findViewById(R.id.tvFMstationname);
+            tvFMstationlink = itemView.findViewById(R.id.tvFMstationlink);
+        }
+
+        public void bind(int position) {
+            FMradiostationsClass station = fmradiostations[position];
+            // Set the station name
+            tvFMstationname.setText(station.getstationname());
+            // Set the station link
+            tvFMstationlink.setText(station.getstationlink());
+            // Set the station image
+            imageView.setImageResource(R.drawable.rvimgstationcontainer); // Assuming you have a default image in your drawable folder
+        }
+    }
+}
